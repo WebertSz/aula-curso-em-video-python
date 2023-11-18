c4a5893c991bec8459c1ec28c4328bc40711afba
WebertSz <trebew2012@gmail.com>
2023-11-18 15:39:55 -0300

    atualização ex097 e 098

---
+++ ex097.py
@@ -0,0 +1,17 @@
+def titulo(txt):
+    print('-=' * 20)
+    print(txt)
+    print('-=' * 20)
+
+
+def escreva(msg):
+    print(f'-' * len(msg))
+    print(msg)
+    print(f'-' * len(msg))
+
+
+titulo("FUNÇÃO - PASSANDO UMA MENSAGEM DE TEXTO")
+escreva("Curso de Python no Youtube")
+escreva("Professor: Guanabara")
+escreva("Curso em vídeo")
+
